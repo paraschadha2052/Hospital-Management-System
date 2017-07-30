57162edf770d307fb4984c8e5d550d27990c22df
Paras Chadha <paraschadha18@gmail.com>
2017-07-30 14:49:04 +0530

    Added Functions in Model

--- src/model/AccountModel.java
+++ src/model/AccountModel.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class AccountModel {
 
-	private String pass = "anshgarg@123";
+	private String pass = "chandigarh";
 
 	
 	public boolean login(String username, String password)
@@ -246,6 +251,9 @@ public class AccountModel {
         public ArrayList<String> getPatientDetails(String username)
 	{
 		ArrayList<String> list = new ArrayList<String>();
+                if(username==null){
+                    return list;
+                }
 		String tmp = "";
 		String query = "select * from patients where uname='"+username+"'";
 		System.out.println(query);
@@ -256,7 +264,9 @@ public class AccountModel {
 			ResultSet rs = stmt.executeQuery();
 			System.out.println(rs);
                         rs.next();
-			for(int i=1;i<=15;i++)
+                        ResultSetMetaData rsmd = rs.getMetaData();
+    			int columnsNumber = rsmd.getColumnCount();
+			for(int i=1;i<=columnsNumber;i++)
 			{
 				tmp = rs.getString(i);
 				list.add(tmp);
@@ -271,6 +281,25 @@ public class AccountModel {
 		return list;
 	}
         
+        public boolean alreadyBookedAppointment(String patientID)
+        {
+            boolean res = true;
+            String query = "select * from appointments where patientID ='"+patientID+"'";
+            System.out.println(query);
+            try{  
+                    Class.forName("com.mysql.jdbc.Driver");     
+                    Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/HMS","root",pass);  
+                    PreparedStatement stmt = con.prepareStatement(query);  
+                    ResultSet rs = stmt.executeQuery();
+                    System.out.println(rs);
+                    res = rs.next();
+            }
+            catch(Exception e)
+            {  
+                    System.out.println(e);  
+            }
+            return res;
+        }
         
         public String unametodocid(String docuname)
         {
@@ -382,4 +411,164 @@ public class AccountModel {
     		}
     		return tmp;
         }
+        
+        public ArrayList<ArrayList<String>> getAvailableDoctorsList(String dept, java.util.Date date)
+    	{
+            System.out.println(date.toString()+" "+dept);
+        	
+            ArrayList<ArrayList<String>> outer = new ArrayList<ArrayList<String>>();
+            String deptId, docId, docName;
+            String query="select * from departments where deptName = '" + dept +"';";
+            
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            String reportDate = df.format(date);
+            java.util.Date today = Calendar.getInstance().getTime();
+           
+            try{  
+                    Class.forName("com.mysql.jdbc.Driver");     
+                    Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/HMS","root",pass);  
+                    PreparedStatement stmt = con.prepareStatement(query);  
+                    ResultSet rs = stmt.executeQuery();
+                    
+                    rs.next();
+                    deptId = rs.getString(1);
+                    
+                    System.out.println(deptId);
+                    
+                    query="select * from doctors where deptID = " + deptId +";";
+                    con.prepareStatement(query);
+                    rs = stmt.executeQuery();
+                    System.out.println(rs);
+                    boolean flag = true;
+                    
+                    while(rs.next()){
+                        docId = rs.getString(1);
+                        docName = rs.getString(2);
+                        String query2 = "select * from DoctorAvailability where docID = " + docId + " and date = '"+reportDate+"';";
+                        PreparedStatement stmt2 = con.prepareStatement(query2);  
+                        ResultSet rs2 = stmt2.executeQuery();
+                        flag = true;
+                        while(rs2.next()){
+                            if(rs2.getInt(2) > 0) {
+                                ArrayList<String> inner = new ArrayList<String>();
+                                inner.add(docId);
+                                inner.add(docName);
+                                for(int i=2;i<=3;i++){
+                                    inner.add(rs2.getString(i));
+                                }
+                                inner.add(deptId);
+                                outer.add(inner);
+                            }
+                            flag = false;
+                        }
+                        
+                        if(flag){
+                            if(date.after(today)){
+                                PreparedStatement stmt3 = con.prepareStatement("insert into DoctorAvailability values("+docId+",15,'"+reportDate+"');");  
+                                stmt3.executeUpdate();
+                                ArrayList<String> inner = new ArrayList<String>();
+                                inner.add(docId);
+                                inner.add(docName);
+                                inner.add("15");
+                                inner.add(reportDate);
+                                inner.add(deptId);
+                                outer.add(inner);
+                            }
+                        }            
+                    }
+           
+                    con.close();
+                    
+            }
+    		catch(Exception e)
+    		{  
+    			System.out.println(e);  
+    		}	
+    		return outer;
+    	}
+        
+        public void addAppointmenttoTable(String patientId, String date, String docID, String deptId, String patientname, int curSlots)
+        {
+                try{
+			String query = "insert into appointments(patientID,date,docID,deptID,patientname) values('" + patientId + "','" + date + "','" + docID + "','" + deptId + "','" + patientname + "')" ;
+			Class.forName("com.mysql.jdbc.Driver");     
+			System.out.println(query);
+			Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/HMS","root",pass);  
+			PreparedStatement stmt = con.prepareStatement(query);  
+			stmt.executeUpdate();
+                        
+                        query = "update DoctorAvailability set slotsAvail ="+(curSlots-1)+" where docID = "+docID+" and date = '"+date+"';";
+                        System.out.println(query);
+                        stmt = con.prepareStatement(query);
+                        stmt.executeUpdate();
+			con.close();
+		}
+		catch(Exception e)
+		{  
+			System.out.println(e);  
+		}
+        }
+        
+        public ArrayList<String> getAppointmentDetailsPatient(String username)
+        {
+            ArrayList<String> list = new ArrayList<String>();
+            System.out.println("**"+username);
+            String pID, depID, docID, appID, tmp;
+            String query = "select * from patients where uname='"+username+"'";
+            System.out.println(query);
+            try{  
+                    Class.forName("com.mysql.jdbc.Driver");     
+                    Connection con = DriverManager.getConnection( "jdbc:mysql://localhost:3306/HMS","root",pass);  
+                    PreparedStatement stmt = con.prepareStatement(query);  
+                    ResultSet rs = stmt.executeQuery();
+                    if(rs.next()){
+                    pID = rs.getString(1);
+                    
+                    query = "select * from appointments where patientID = '"+pID+"';";
+                    stmt = con.prepareStatement(query);
+                    System.out.println(query);
+                    rs = stmt.executeQuery();
+                    if(rs.next()){
+                        list.add(rs.getString(1));
+                        list.add(rs.getString(3));
+                        
+                        docID = rs.getString(4);
+                        depID = rs.getString(5);
+                        
+                        query = "select * from doctors where docID = '"+docID+"';";
+                        System.out.println(query);
+                        stmt = con.prepareStatement(query);
+                        rs = stmt.executeQuery();
+                        if(rs.next()){
+                            list.add(rs.getString(2));
+                        } else {
+                            list.add("");
+                        }
+                    
+                        query = "select * from departments where deptID = '"+depID+"';";
+                        System.out.println(query);
+                        stmt = con.prepareStatement(query);
+                        rs = stmt.executeQuery();
+                        if(rs.next()){
+                            list.add(rs.getString(2));
+                        } else {
+                            list.add("");
+                        }
+                    
+                    } else {
+                        for(int i=0;i<4;i++){
+                            list.add("");
+                        }
+                    }
+                    }
+                    con.close();
+
+            }
+            catch(Exception e)
+            {  
+                    System.out.println(e);  
+            }
+            return list;
+        }
+        
 }
